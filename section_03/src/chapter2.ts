147e38bf82f0dcce017b75160de2cb9a2d028f51
Uichae <ghddmlco@gmail.com>
2023-07-20 20:58:45 +0900

    typescript

---
+++ section_03/src/chapter2.ts
@@ -0,0 +1,9 @@
+// Unknown 타입 => 모든 타입의 슈퍼 타입
+
+function UnknownExam() {
+  let a: unknown = 1;
+  let b: unknown = "hello";
+  let c: unknown = true;
+  let d: unknown = null;
+  let e: unknown = undefined;
+}
